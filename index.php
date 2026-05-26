0c6fb17c74b3468d9bb3e8ff33d07150d41ec093
isabela728 <isabela_oliveira3@estudante.sesisenai.org.br>
2026-05-26 12:03:38 -0300

    add - php para validação e cadastro de usuario

--- index.php
+++ index.php
@@ -1,52 +1,64 @@
 <?php
-
 session_start();
 
-$host = "localhost";
-$user = "root";
-$pass = "root";
-$db = "sistema_simples";
+include("conexao_db.php");
 
-$conn = new mysqli($host,$user,$pass,$db);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
 
-if($conn->connect_error){
-    die("Erro na conexão");
-}else{
-    // echo "<p>Banco:OK</p>";
-    echo "<script>console.log('Banco:OK');</script>";
-}
+    $sql = "SELECT * FROM usuario WHERE usuario = '$usuario' AND senha = '$senha' ";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){  //quando no seu servidor tipo de requisição for POST
-    $usuario = $_POST["usuario"]; //recebe usuario
-    $senha = $_POST["senha"]; //recebe senha
-}
+    $resultado = $conn->query($sql);
+
+   if($resultado -> num_rows > 0){
+
+    $_SESSION["usuario"] = $usuario;
+    header("Location: home.php");
+    exit();
+
+   }else{
+    $erro = "Usuário ou senha inválido.";
+   }
+
+};
 
 ?>
-
-
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login com o Banco</title>
+    <title>Login com o Banco</title>
 </head>
+<style>
+    #erro{
+        color:red;
+    }
+</style>
 <body>
-
-    <h2>Login com PHP</h2>
-
+    <h2>
+        Login com PHP
+    </h2>
+    
     <form method="POST">
-        <label for="usuario">Usuário</label>
+        <label for="usuario">Usuario</label>
         <input type="text" name="usuario">
-
         <br><br>
-
         <label for="senha">Senha</label>
         <input type="password" name="senha">
+        <br>
+        <?php
+        
+        if(isset($erro)){
+            echo "<p id='erro'>$erro</p>";
+        };
 
-        <br><br>
+        ?>
+        
+        <br>
+
         <button type="submit">Entrar</button>
-
     </form>
 
 </body>
